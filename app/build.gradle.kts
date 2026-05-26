41cd458dfaf4f7161b445f741d60f4afe724e359
eddyminy <L22270513@TUXTLA.TECNM.MX>
2026-05-26 00:23:35 -0600

    Configura Firebase y base visual de la app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,10 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
+    implementation("androidx.credentials:credentials:1.3.0")
+    implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
 
 
     implementation(libs.androidx.activity.ktx)
